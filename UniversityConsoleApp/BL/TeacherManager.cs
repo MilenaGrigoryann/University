a4d48b20d63094529158a7c5a26530092c090b5a
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-21 09:18:34 +0400

    Add files via upload

---
+++ UniversityConsoleApp/BL/TeacherManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UniversityConsoleApp.Models;
+using UniversityConsoleApp.Interfaces;
+using System.Collections.Generic;
+
+namespace UniversityConsoleApp.BL
+{
+    public class TeacherManager : ICreate, IPrint
+    {
+        int maxAge = 89;
+
+        public Person Create(string firstName, string lastName, int age)
+        {
+            return new Teacher(firstName, lastName, age);
+        }
+
+
+        public void Print(Person person)
+        {
+            Console.WriteLine("**********Teacher**********");
+            Console.WriteLine($"id:{person.ID} name:{person.FirstName} lastName:{person.LastName} age:{person.Age}");
+            if (person is Teacher)
+            {
+                if (((Teacher)person).Students == null)
+                    Console.WriteLine("___________________________________________");
+                else
+                {
+                    for (int i = 0; i < ((Teacher)person).Students.Count; i++)
+                        Console.WriteLine($"Student_id:{((Teacher)person).Students[i].ID} name:{((Teacher)person).Students[i].FirstName} lastName:{((Teacher)person).Students[i].LastName} age:{((Teacher)person).Students[i].Age}");
+                }
+
+            }
+
+        }
+
+
+        public void Print(List<Person> people)
+        {
+            for (int i = 0; i < people.Count; i++)
+            {
+                Print(people[i]);
+
+            }
+
+        }
+
+        public List<Person> Create(int count, int minAge)
+        {
+            List<Person> teachers = new List<Person>(count);
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                teachers.Add(new Teacher($"name{i}", $" lastName{i}", rnd.Next(minAge, maxAge)));
+            }
+            return teachers;
+
+        }
+    }
+}
---
+++ UniversityConsoleApp/BL/UniversityManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UniversityConsoleApp.Models;
+using System.Collections.Generic;
+namespace UniversityConsoleApp.BL
+{
+    public static class UniversityManager
+    {
+        public static Student SwapWithTeacher(Student student, Teacher teacher)
+        {
+            student.Teacher = teacher;
+            return student;
+        }
+        public static List<Person> SwapWithTeachers(List<Person> students, List<Person> teachers)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Student student = (Student)students[i];
+                Teacher teacher = (Teacher)teachers[i % teachers.Count];
+                student.Teacher = teacher;
+
+            }
+            return students;
+        }
+        public static List<Person> SwapWithStudent(List<Person> teachers, List<Person> students)
+        {
+            int scount = students.Count;
+            int tcount = teachers.Count;
+
+            for (int i = 0; i < teachers.Count; i++)
+            {
+                int perTeacher = scount / tcount;
+                Teacher teacher = (Teacher)teachers[i];
+                teacher.Students = new List<Student>(perTeacher);
+                for (int j = 0; j < perTeacher; j++)
+                {
+                    teacher.Students.Add((Student)students[j]);
+                }
+
+                scount = scount - perTeacher;
+                tcount--;
+
+            }
+
+            return teachers;
+        }
+        public static Teacher SwapWithStudents(Teacher teacher, List<Student> students)
+        {
+            teacher.Students = students;
+            return teacher;
+        }
+        public static List<Person> SwapWithGroup(List<Person> students, Group group)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Student student = (Student)students[i];
+                student.Group = group;
+            }
+            return students;
+        }
+
+    }
+}
