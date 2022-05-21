7881b09f6ea9f2b8ba0f825fff4c5e1cf6e9976b
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-21 09:20:36 +0400

    Add files via upload

---
+++ UniversityConsoleApp/Models/Teacher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace UniversityConsoleApp.Models
+{
+    public class Teacher : Person
+    {
+        public List<Student> Students { get; set; }
+        public Group Group { get; set; }
+        public Group[] Groups { get; set; }
+        public Teacher()
+        {
+
+        }
+        public Teacher(string firstName, string lastName, int age) : base(firstName, lastName, age)
+        {
+
+        }
+        public Teacher(string firstName, string lastName, int age, List<Student> students) : base(firstName, lastName, age)
+        {
+
+            Students = students;
+
+        }
+        public Teacher(string firstName, string lastName, int age, List<Student> students, Group group) : base(firstName, lastName, age)
+        {
+
+            Students = students;
+
+            Group = group;
+
+        }
+
+    }
+}
